cdf45f2967efad05e96a327fad377980ea8a712d
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2017-10-12 22:17:52 +0200

    Klar for CapraCon 2017!

--- presentation/index.js
+++ presentation/index.js
@@ -5,6 +5,7 @@ import { withProps } from "recompose";
 
 // Import Spectacle Core tags
 import {
+  Spectacle,
   Appear,
   Deck,
   BlockQuote,
@@ -37,7 +38,7 @@ require("spectacle/lib/themes/default/index.css");
 // Try to match the Capra color palette (best effort)
 const theme = createTheme(
   {
-    primary: "white",
+    primary: "#F6F4F4",
     secondary: darken(0.1, "#60C7E8"),
     tertiary: "#0059AC",
     quartenary: "#010121"
@@ -78,7 +79,19 @@ const SubTitle = withStyle({
 
 const Presentation = () => (
   <Deck transition={["slide"]} transitionDuration={500} theme={theme}>
-    <Slide>
+    <Slide
+      notes={`
+      <ul>
+        <li>Noe litt meta Onsdag: Fokus på hva man ønsker å formidle.</li>
+        <li>
+          Foredraget er IKKE et innsalg, heller åpne opp for refleksjon.
+        </li>
+        <li>Tutorials kan man kikke på selv osv osv,</li>
+        <li>Et verktøy/arbeidsmåte man bør vurdere å ta i bruk hos kunde.</li>
+        <li>Ønsker å se saken fra to sider, evt 1.5 side.</li>
+      </ul>
+      `}
+    >
       <Heading size={1}>Typer i frontendverden</Heading>
       <SubTitle>- En nødvendighet eller unødvendig?</SubTitle>
     </Slide>
@@ -87,6 +100,11 @@ const Presentation = () => (
         <Heading size={2}>Gir det verdi?</Heading>
       </Appear>
     </Slide>
+
+    <Slide notes="Superkort intro. Mange kjenner meg alt, og andre kan bli bedre kjent med meg etterrpå!">
+      <Text>Jeg heter Jonas og skriver kode.</Text>
+    </Slide>
+
     <Slide textColor="black" notes="Inndeling, fordeler, ulemper, eksempler">
       <Heading size={2}>
         Agenda{" "}
@@ -106,6 +124,9 @@ const Presentation = () => (
         </Appear>
         <Appear>
           <ListItem>Eksempler</ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>Diskusjon</ListItem>
         </Appear>
       </List>
     </Slide>
@@ -250,6 +271,36 @@ const Presentation = () => (
     </Slide>
 
     <Slide>
+      <List>
+        <ListItem>Statisk typet språk</ListItem>
+        <ListItem>Veldig kraftig type-inferens</ListItem>
+        <ListItem>
+          Constructs man ofte savner i JavaScript: Union types, exhaustive
+          switch++
+        </ListItem>
+      </List>
+    </Slide>
+
+    <CodeSlide
+      bgColor="quartenary"
+      align="center flex-start"
+      code={require("raw-loader!../assets/examples/reason_example2.re.example")}
+      lang="reason"
+      ranges={[
+        {
+          loc: [0, 3],
+          note: "Union types!"
+        },
+        {
+          loc: [5, 7]
+        },
+        {
+          loc: [8, 13]
+        }
+      ]}
+    />
+
+    <Slide>
       <Text textSize="4rem" margin="2rem">
         Now what?
       </Text>
@@ -260,10 +311,16 @@ const Presentation = () => (
       </Appear>
     </Slide>
 
-    <Slide />
-    {/* <Slide>
-        <Image height="30rem" src={images.money} />
-        </Slide> */}
+    <Slide>
+      <Text textColor="firebrick">Fordi vi kan?</Text>
+      <Text textColor="firebrick">Det ser kult ut?</Text>
+      <Appear>
+        <Text textColor="forestgreen" margin="2rem">
+          Det gjør prosjekter enklere å vedlikeholde
+        </Text>
+      </Appear>
+    </Slide>
+
     <Slide>
       <Image
         style={{
@@ -284,6 +341,53 @@ const Presentation = () => (
         </Quote>
         <Cite textColor="goldenrod">Ken Wheeler</Cite>
       </BlockQuote>
+    </Slide>
+
+    <Slide notes="SPA, mange kodelinjer versus enkle scripts for CI. Biblioteker. <br />Gjenbruk av kode i store organisasjoner">
+      <Text>Skal vi bruke slike typesystemer i prosjektene våre?</Text>
+
+      <Appear>
+        <Text margin="2rem" textColor="forestgreen">
+          Det kommer an på!
+        </Text>
+      </Appear>
+    </Slide>
+
+    {/* <Slide>
+        <Text>Pitfalls</Text>
+        <Appear>
+        <List>
+        <ListItem>Bruk av Any-annoteringer</ListItem>
+        <ListItem>"Bruk som du vil"</ListItem>
+        <ListItem>Optionals</ListItem>
+        </List>
+        </Appear>
+        </Slide> */}
+
+    <Slide transition={["spin"]}>
+      <Heading size={2}>Quiz!</Heading>
+    </Slide>
+
+    <Slide>
+      <List>
+        <Appear>
+          <ListItem>Et byggskript</ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>Et bibliotek som konsumeres av folket</ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>En Single-Page app, stor kodebase</ListItem>
+        </Appear>
+      </List>
+    </Slide>
+
+    <Slide>
+      <Heading size={2}>Spørsmål/Diskusjon</Heading>
+    </Slide>
+
+    <Slide>
+      <Heading size={1}>Takk for meg!</Heading>
     </Slide>
   </Deck>
 );
